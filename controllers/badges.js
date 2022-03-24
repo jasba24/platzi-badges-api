ff39412c53349828458bef46ef9c5fe8e665db00
Hepo-Inpu <juammarin@misena.edu.co>
2022-03-24 11:20:55 -0500

    add put and delete methods

--- controllers/badges.js
+++ controllers/badges.js
@@ -12,4 +12,20 @@ badgeRouter.post('/', async (req, res) => {
   res.status(201).json(savedBadge)
 })
 
+badgeRouter.put('/:id', async (req, res, next) => {
+  const id = req.params.id
+  const newBadge = req.body
+
+  Badge.findByIdAndUpdate(id, newBadge, {new: true}).then(result => {
+    res.json(result)
+  }).catch(err => next(err))
+})
+
+badgeRouter.delete('/:id', async (req, res, next) => {
+  const id = req.params.id
+
+  await Badge.findByIdAndRemove(id)
+  res.status(204).end()
+})
+
 module.exports = badgeRouter
